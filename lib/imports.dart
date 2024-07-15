6e9f8cd05a0430a7839735a564cc79421e4b4dfd
Alexander <vsany@meta.ua>
2024-07-15 15:04:08 +0300

    update global imports

---
+++ lib/imports.dart
@@ -0,0 +1,5 @@
+export 'package:flutter/material.dart';
+export 'dart:async';
+
+export 'package:kabanchik_app/screens/auth_screen.dart';
+export 'package:kabanchik_app/screens/registration_screen.dart';
