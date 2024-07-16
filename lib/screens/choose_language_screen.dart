d4328f4af5c05fc84eef8e9ebde517effae68e16
Alexander <vsany@meta.ua>
2024-07-16 18:03:59 +0300

    in work ChooseLanguageScreen

---
+++ lib/screens/choose_language_screen.dart
@@ -0,0 +1,10 @@
+import '../imports.dart';
+
+class ChooseLanguageScreen extends StatelessWidget {
+  const ChooseLanguageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
