69091eaa8dc8604cb478f4bc9e4a1208a5f3ad16
Alexander <vsany@meta.ua>
2024-07-23 00:00:08 +0300

    select client type. 50 \ 50

--- lib/screens/select_client_type_screen.dart
+++ lib/screens/select_client_type_screen.dart
@@ -1,20 +1,59 @@
 import '../imports.dart';
 
-class SelectClientTypeScreen extends StatelessWidget {
-  const SelectClientTypeScreen({super.key});
+class SelectClientTypeScreen extends StatefulWidget {
+  const SelectClientTypeScreen({Key? key}) : super(key: key);
+
+  @override
+  _SelectClientTypeScreenState createState() => _SelectClientTypeScreenState();
+}
+
+class _SelectClientTypeScreenState extends State<SelectClientTypeScreen> {
+  bool? isUserClient;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const SafeArea(
-          child: Column(
-            children: [
-                Padding(padding: EdgeInsets.only( top: 20),
-                child: Text('User Registration',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ))
-            ],
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('wwwroot/Images/background.jpg'),
+            fit: BoxFit.cover,
+            opacity: 0.1,
+          ),
+        ),
+        child: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Center(
+                            child: Text(
+                              'User Registration',
+                              style: TextStyle(
+                                fontSize: constraints.maxWidth * 0.06,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Expanded(
+                          flex: 4,
+                          child: Center(
+                            
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ),
