2874ad09a6b57bea75f820d598186c4fa8753d9d
Alexander <vsany@meta.ua>
2024-08-06 18:07:28 +0300

    [*] navigation buttons update

--- lib/widgets/screen_navigation_widget.dart
+++ lib/widgets/screen_navigation_widget.dart
@@ -14,31 +14,28 @@ class ScreenNavigationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 55.0, vertical: 73.0),
-      child: Container(
-        width: 110,
-        height: 40,
-        decoration:  BoxDecoration(
-          color: isFurtherEnabled
-              ? const Color.fromRGBO(116, 192, 188, 1)
-              : const Color.fromRGBO(203, 233, 233, 1),
-          borderRadius: BorderRadius.zero,
+    return Container(
+      width: 110,
+      height: 40,
+      decoration: BoxDecoration(
+        color: isFurtherEnabled
+            ? const Color.fromRGBO(116, 192, 188, 1)
+            : const Color.fromRGBO(203, 233, 233, 1),
+        borderRadius: BorderRadius.zero,
+      ),
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          backgroundColor: Colors.transparent,
         ),
-        child: TextButton(
-          onPressed: onPressed,
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
-            backgroundColor: Colors.transparent,
-          ),
-          child: Text(
-            text,
-            style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontFamily: 'Rokkitt'
-            ),
+        child: Text(
+          text,
+          style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontFamily: 'Rokkitt'
           ),
         ),
       ),
